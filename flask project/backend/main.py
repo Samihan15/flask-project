1174ac8ac59bfedcfc08bbee6349484979298afb
Samihan <samihannandedkar15@gmail.com>
2024-05-09 09:04:08 +0530

    minor changes

--- flask project/backend/main.py
+++ flask project/backend/main.py
@@ -69,5 +69,5 @@ def login():
         return jsonify({'success': False, 'message': 'Invalid request'})
 
 if __name__ == '__main__': 
-    app.run(port=3000,host='0.0.0.0') 
+    app.run(port=3000) 
     
